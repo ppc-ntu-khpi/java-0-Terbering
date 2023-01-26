437d50a9785d1daf89ab9255fa8d3a0f2d9bfe9e
Terbering <113172992+Terbering@users.noreply.github.com>
2023-01-26 20:58:20 +0200

    Add files via upload

---
+++ src/Quotation.java
@@ -0,0 +1,10 @@
+
+public class Quotation {
+
+    String quote = "Peace begins with a smile.";
+    String var = "Mother Teresa";
+    public void display() {
+        System.out.println(quote);
+        System.out.println(var);
+    }
+}
---
+++ src/Shirt.java
@@ -0,0 +1,19 @@
+
+public class Shirt {
+
+    public int shirtID = 1; // стандартне значення номера моделі сорочки
+    public String description = "-plain t-shirt-"; // стандартний опис сорочки
+    // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
+    public char colorCode = 'R';
+    public double price = 9.99; // стандартна вартість сорочки
+    public int quantityInStock = 1000; // стандартна кількість на складі
+
+    // цей метод просто виводить всю інформацію про сорочку на екран
+    public void displayShirtInformation() {
+        System.out.println("Shirt ID: " + shirtID);
+        System.out.println("Shirt description:" + description);
+        System.out.println("Color Code: " + colorCode);
+        System.out.println("Shirt price: $" + price);
+        System.out.println("Quantity in stock: " + quantityInStock);
+    } // кінець методу displayShirtInformation
+} // кінець опису класу
